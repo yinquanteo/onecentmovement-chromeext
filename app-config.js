c0c8085d8f73e8908ef7d227df90823e6b6a6499
Yinquan Teo <yinquan.teo@gmail.com>
2013-10-15 18:24:12 +0800

    change endpoints from herokuapp.com to onecentmovement.org

--- app-config.js
+++ app-config.js
@@ -9,8 +9,8 @@ if (ONECENT_DEVELOPMENT_MODE) {
 } else {
   var ONECENT_CONFIG = {
     popUpDelay: 2000,
-    donationEndPoint: "http://onecent.herokuapp.com/round_ups.json",
-    authTokenEndPoint: "http://onecent.herokuapp.com/tokens",
-    signUpLink: "http://onecent.herokuapp.com/register"
+    donationEndPoint: "http://onecentmovement.org/round_ups.json",
+    authTokenEndPoint: "http://onecentmovement.org/tokens",
+    signUpLink: "http://onecentmovement.org/register"
   };
 }
